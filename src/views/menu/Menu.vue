d87d7c774a071282f25ba7312b2cfbf4110d68c0
Clarence Coronel <clarence.coronel.r@gmail.com>
2024-05-14 12:39:00 +0800

    feat: add personal footer

--- src/views/menu/Menu.vue
+++ src/views/menu/Menu.vue
@@ -72,7 +72,7 @@
         >
           <img
             loading="eager"
-            class="max-w-[700px] w-full object-fit object-center"
+            class="max-w-[600px] w-full object-fit object-center"
             src="https://1000logos.net/wp-content/uploads/2023/05/Straw-Hat-Logo.png"
             alt=""
           />
@@ -114,6 +114,32 @@
       </template>
 
       <Leaderboard v-else-if="activeMenu == 'LEADERBOARD'" />
+      <div class="w-full h-10 text-white text-center">
+        <a
+          href="https://github.com/clarence-coronel"
+          target="_blank"
+          class="w-fit mx-auto duration-500 group flex gap-2 justify-center"
+        >
+          <span
+            >Copyright &copy; {{ new Date().getFullYear() }} Clarence
+            Coronel</span
+          >
+          <svg
+            class="group-hover:rotate-[360deg] duration-1000"
+            xmlns="http://www.w3.org/2000/svg"
+            width="1.5rem"
+            height="1.5rem"
+            viewBox="0 0 15 15"
+          >
+            <path
+              fill="currentColor"
+              fill-rule="evenodd"
+              d="M7.5.25a7.25 7.25 0 0 0-2.292 14.13c.363.066.495-.158.495-.35c0-.172-.006-.628-.01-1.233c-2.016.438-2.442-.972-2.442-.972c-.33-.838-.805-1.06-.805-1.06c-.658-.45.05-.441.05-.441c.728.051 1.11.747 1.11.747c.647 1.108 1.697.788 2.11.602c.066-.468.254-.788.46-.969c-1.61-.183-3.302-.805-3.302-3.583a2.8 2.8 0 0 1 .747-1.945c-.075-.184-.324-.92.07-1.92c0 0 .61-.194 1.994.744A6.963 6.963 0 0 1 7.5 3.756A6.97 6.97 0 0 1 9.315 4c1.384-.938 1.992-.743 1.992-.743c.396.998.147 1.735.072 1.919c.465.507.745 1.153.745 1.945c0 2.785-1.695 3.398-3.31 3.577c.26.224.492.667.492 1.343c0 .97-.009 1.751-.009 1.989c0 .194.131.42.499.349A7.25 7.25 0 0 0 7.499.25"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </a>
+      </div>
     </div>
   </div>
   <div
@@ -154,7 +180,7 @@
     </div>
     <div class="flex flex-col w-full h-full justify-between items-center">
       <img
-        class="max-w-[700px] w-full object-fit object-center"
+        class="max-w-[600px] w-full object-fit object-center"
         src="https://1000logos.net/wp-content/uploads/2023/05/Straw-Hat-Logo.png"
         alt=""
       />
